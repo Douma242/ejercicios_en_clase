07db06d16817184c0f00f74f915880d4cca37ddd
Douma242 <danieldouma@hotmail.es>
2016-06-08 09:41:50 -0600

    falta promedio y limites para terminar

---
+++ registro.py
@@ -0,0 +1,22 @@
+print ("Bienvenido")
+estudiantes= {}
+opc= 0
+while opc!=4:
+	opc= int(input("1.crear estudiante \n2.ingresar nota \n3.reporte de notas \n4.Salir \n"))
+	if opc== 1:
+		estudiantes={
+		input("Ingrese nombre "):[]
+		}
+	elif opc==2:
+		opc2="si"
+		nombre= input("Ingrese estudiante: ")
+		while opc2=="si":
+			notas= int(input("Ingrese nota: "))
+			estudiantes[nombre].append(notas)
+			opc2= input("desea seguir? ")
+	elif opc==3:
+		nombre= input("Ingrese estudiante: ")
+		for nota1 in estudiantes[nombre]:
+			print(nota1)
+print("Bai Bai")
+
