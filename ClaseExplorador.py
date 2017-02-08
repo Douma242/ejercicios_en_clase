e3f54829f0272eb166ac76a572110cbbe94c3684
Douma242 <danieldouma@hotmail.es>
2017-02-08 08:44:35 -0600

    :=)

--- ClaseExplorador.py
+++ ClaseExplorador.py
@@ -1,19 +1,23 @@
 class Explorer(object):
-	def __init__(self, nombre):
+	def __init__(self, nombre, tabs, url):
 		self.nombre = nombre
 		self.tabs= []
+		self.urls= []
 	
 
-	def agregar_tab(self, nombres):
+	def agregar_tab(self, nombres, url):
 		self.tabs.append(nombres)
+		self.urls.append(url)
 
 	def mostrartabs(self):
 		for i in range (0, len(self.tabs)):
 			return self.tabs[i]
+	
 	def eliminartab(self, nombres):
-		self.tabs.pop(nommbres)
+		self.tabs.pop(nommbres+1)
+		self.urls.pop(nommbres+1)
 
 	def eliminartodas(self):
-		self.tabs=
+		self.tabs= False
 
 
--- ClaseTab.py
+++ ClaseTab.py
@@ -1,4 +1,5 @@
 class TabTab(object):
-	def __init__(self, tab):
+	def __init__(self, tab, url):
 		self.tab = tab
+		self.url = url
 		
